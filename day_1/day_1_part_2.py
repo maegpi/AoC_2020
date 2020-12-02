374de5c28bd4dc06945d775f9699676b782190fc
maegpi <ma.emshey@gmail.com>
2020-12-01 21:38:57 -0330

    removed comments and updated info header

--- day_1/day_1_part_2.py
+++ day_1/day_1_part_2.py
@@ -1,6 +1,6 @@
 #maegpi
 #2020-12-01
-#AoC Day 1, Part 1
+#AoC Day 1, Part 2
 
 
 getNums = open('day_1_nums.txt', 'r')
@@ -16,7 +16,6 @@ num_bank_2 = {}
 
 counter_1 = 0
 counter_2=0
-counter_3= 0
 
 for i in range(len(lst)):
     if lst[i] < FINAL_ANSWER/2:
@@ -25,8 +24,6 @@ for i in range(len(lst)):
     else:
         num_bank_2[counter_2] = lst[i]
         counter_2 += 1
-#print(num_bank_1,"\n")
-#print(num_bank_2)
 
 
 check = False
@@ -39,17 +36,3 @@ while check is not True:
                     check = True
 
 print("The three values are: ",winVal,"\nAnd the answer is: ",winVal[0]*winVal[1]*winVal[2])
-"""
-list_two = []
-check = False
-while check is not True:
-    for j in range(len(num_bank_2)):
-        for k in range(len(num_bank_1)):
-            if num_bank_1[k] + num_bank_2[j] == FINAL_ANSWER:
-                list_two = [num_bank_1, num_bank_2]
-                
-print("\n\n\n\nprinting new list!\n")
-print(list_two)
-
-#print("The two values are: ", winVal,"\n the answer is: ",winVal[0]*winVal[1])
-"""
